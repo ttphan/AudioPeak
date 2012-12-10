86e738c8971ba81d7dff492597f58562022dac97
Kees Lampe <kees.lampe@gmail.com>
2012-12-10 15:28:18 +0100

    serchall splitsen naar htmlDT.js

--- js/searchAll.js
+++ js/searchAll.js
@@ -9,10 +9,7 @@ $(document).ready(function() {
 	});  
 });
 
-var YTplayerState = 0;
-var playing = 1;
-var playButton = '<img src="images/playbutton.png" width="15" height="15" alt="play">'
-var playNextButton = '<img src="images/playnextbutton.png" width="15" height="15" alt="playNext">'
+
 var playList = new Array();
 
 function track(name,artist,image){
@@ -39,8 +36,6 @@ function searchLastfm(search) {
 	//zoek gebruik makende van seachall_ajaxjson.php welke een json object echo't.
 	$.ajax({
 		type: "GET",
-//		url: "tests/seachAll_ajaxjson.php",
-//		data: "search="+search,
 		url: "php/ajax.php",
 		data: {search : search},
 		cache: false,
@@ -50,27 +45,9 @@ function searchLastfm(search) {
 		},
     	error: function (xhr, ajaxOptions, thrownError) {
 			//TODO goed afhandelen.
-			$('#ResultsDiv').html('Error: Mogelijk geen resultaten gevonden in Last.fm');
+			$('#ResultsDiv').html('Error: Waarschijnlijk iets met php');
       	}
 	});
-}
-
-function showSearchResults(json){
-	//maak resulsdiv leeg
-	$('#ResultsDiv').html('');
-		
-	//haal vars uit json opject plaats deze in de resultsdiv met een span id van i. i wordt doorgegeven aan seachYT om daar de youtube play link te plaatsen
-	//ook de image var uit deze json wordt doorgegevn zodat showButton deze ook kan plaatsen
-	window.numberResults = json.length; //door hem aan window vast te maken wordt ie global
-	for (var i=0;i<numberResults;i++) { 		
-		myTrack = new track(json[i].trackName_php,
-							json[i].artist_php,
-							json[i].trackImage_php
-							);
-							
-		$('#ResultsDiv').append('<span id=\"' + i + '\" ></span> ' + myTrack.artist + ' - ' + myTrack.name + '<br>' );
-		searchYT(myTrack, i)
-	}	
 }
 
 function searchYT(myTrack, i){	
@@ -95,118 +72,47 @@ function searchYT(myTrack, i){
 			'&fmt=18' +
 			'&max-results=1' +
 			'&orderby=relevance' +
-			'&key=AI39si7Blv0HpIGbcHtzjaS70mFR-XEcomtJFHQcKC1-4yEthFsx4AhkMFldBeE_5UyD9jEEFCPMt2jzxDLF3hPT1SRoi1La4Q', function(data) {
+			'&key=AI39si7Blv0HpIGbcHtzjaS70mFR-XEcomtJFHQcKC1-4yEthFsx4AhkMFldBeE_5UyD9jEEFCPMt2jzxDLF3hPT1SRoi1La4Q', function(data) {				
 				showButtons(data, i, myTrack);
 			});				
 }
 
-// See https://developers.google.com/youtube/2.0/developers_guide_jsonc#Understanding_JSONC for the feed information
-function showButtons(transport, j, myTrack) {
-	var entries	= transport.data.items || [];
-	
-	if (entries[0] === undefined){
-		var button = '<a href=\"#\">NoYT</a>';
-		$('#'+j).append(' ' + button);
-	}
-	else{
-		//haal de id van de video uit de transport data.
-		var id = entries[0].id;
-		
-		//plaats een play button als de ytplayer klaar is met afspelen	
-		if (YTplayerState == 0){
-			var button = '<a href=\"javascript:createVideo(\''+ id +'\',\''+ myTrack + '\')\">'+ playButton + '</a>';
-			$('#'+j).html(button);
-		}
-		//plaats een playnext button als e ytplayer nog aan het afspelen is
-		else {
-			var button = '<a href = \"javascript:playNext(\''+ id +'\',\''+ myTrack + '\')\">'+ playNextButton + '</a>';
-			$('#'+j).html(button);
-		}
-	}
-}
 
-//laat de video zien als er op lay wordt gedrukt, en plaats de image.
-//TODO eartist, trackName info en extra info laten zienvvvvv
-function createVideo(vidId, name,artist,image) {
-	currentTrack = new track(name,artist,image);
-
-	if(currentTrack.image == ''){
-		//zoek op Plasticman Cha Vocal die heeft geen afbeelding
-		$('#album').attr('src','images/albumgeen.jpg');
-		$('#huidigenummerbalk').attr('src','images/albumgeen.jpg');		
-	}
-	else{
-		$('#album').attr('src',currentTrack.image);
-		$('#huidigenummerbalk').attr('src',currentTrack.image);
-	}
-	$('#artist').html('<a href = \"javascript:searchLastfm(\'' + currentTrack.artist + '\')\">' + currentTrack.artist + '</a>');
-	$('#trackName').html(currentTrack.name);
-	createExtraInfo(name,artist);
-		
-	$('#playVideoDiv').flash({	
-			id: 'ytplayer',
-			swf: 'http://www.youtube.com/v/' + vidId + '?enablejsapi=1&playerapiid=ytplayer&version=3&autoplay=1',
-			width: 425,
-			height: 356,
-			allowScriptAccess: 'always',
-		}
-	);	
-	
-	//maak van de playbuttons play next buttons er is nu tenslotte iets aan het spelen.	
-	for (var i=0;i<numberResults;i++) { 					
-		var buttonhtml = $('#'+i).html();
-		split = buttonhtml.split("'");
-		
-		var id = split[1];
-		myTrack = new track(split[3],split[5],split[7]);
-		
-		var button = '<a href = \"javascript:playNext(\''+ id +'\',\''+ myTrack + '\')\">'+ playNextButton + '</a>';
-		$('#'+i).html(button);
-	}
-	playing = 0;
-	
-	//TODO doe ajax request voor extra info en plaats deze.
-}
 function createExtraInfo(name,artist) {
 	
 		var track = [artist, name];
 		console.log(track);
 	
-	$.ajax({
-		type: "GET",
-		url: "php/ajax.php",
-		data: {getInfo : track},
-		dataType: 'json',
-		succes: function(result) {
-			console.log('deze shit werkt');
-			//showExtraInfo(result);
-		}
-	});
-	
-	//var track = [artist, name];
 //	$.ajax({
 //		type: "GET",
 //		url: "php/ajax.php",
 //		data: {getInfo : track},
-//		cache: false,
-//		dataType: 'json',
-//		success: function(json) {
-//			console.log(json);
-//		},
-//    	error: function (xhr, ajaxOptions, thrownError) {
-//			//TODO goed afhandelen.
-//			$('#ResultsDiv').html('Error: Mogelijk geen resultaten gevonden in Last.fm');
-//      	}
+////		dataType: 'json',
+//		succes: function(result) {
+//			console.log('deze shit werkt');
+//			//showExtraInfo(result);
+//		}
 //	});
+	
+	$.ajax({
+		type: "GET",
+//		url: "tests/seachAll_ajaxjson.php",
+//		data: "search="+search,
+		url: "php/ajax.php",
+		data: {getInfo : track},
+		cache: false,
+		dataType: 'json',
+		success: function(json) {
+			showExtraInfo(json);
+		},
+    	error: function (xhr, ajaxOptions, thrownError) {
+			//TODO goed afhandelen.
+			$('#ResultsDiv').html('Error: Mogelijk geen resultaten gevonden in Last.fm');
+      	}
+	});
 }
 
-function showExtraInfo(json){
-	//maak resulsdiv leeg
-	$('#wikiSum').html('hoi');
-	console.log('json ding:' + json);
-	
-	$('#wikiSum').html(json['album'].wiki);	
-}
+
 
 
 //volgende nummer
